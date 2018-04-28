8cf2c127ded1b1a1a417a6c2614bfc182ef9b9a3
Carlos Michael Rodriguez <cmr624@nyu.edu>
2018-04-28 16:32:29 -0400

    add my stuff

--- sketch.js
+++ sketch.js
@@ -6,16 +6,19 @@ var markerHiro, markerZb;
 
 let fontRussian;
 
+let ryabImage01;
+
 function preload()
 {
   fontRussian = loadFont("fonts/russian.ttf");
+  ryabImage01 = loadImage("images/RYAB_006.jpg");
 }
 
 
 function setup() {
   // create our world (this also creates a p5 canvas for us)
   world = new World('ARScene');
-
+  imageMode(CENTER);
   // grab a reference to our two markers that we set up on the HTML side (connect to it using its 'id')
   markerHiro = world.getMarker('hiro');
   markerZb = world.getMarker('zb');
@@ -27,22 +30,33 @@ function draw() {
   world.clearDrawingCanvas();
 
   // use the markers as positional controllers
-  if (markerZb.isVisible() == true) {
+  if (markerHiro.isVisible() == true)
+  {
     // get the position of this marker
-    var zbPosition = markerZb.getScreenPosition();
+    var hiroPosition = markerHiro.getScreenPosition();
 
-    // draw an ellipse here
-    fill(0,255,0);
-    stroke(0);
-    strokeWeight(5);
-    ellipse( zbPosition.x, zbPosition.y, 50, 50 );
-    strokeWeight(1);
-    textFont(fontRussian);
-    //RUSSIAN FAILS
-    text("Евгения Самойловна Рябая родилась 6 марта 1919 года витута. После начала войны эвагом в госпитале" + zbPosition.x + ", " + zbPosition.y, zbPosition.x, zbPosition.y+50);
     //HEBREW FAILS
     //text("יטרי בחטיבת הרובים " + zbPosition.x + ", " + zbPosition.y, zbPosition.x, zbPosition.y+50);
-
+    text("hey!!!", hiroPosition.x, hiroPosition.y, hiroPosition.y+50);
+    let s1 = new Slide(hiroPosition.x, hiroPosition.y, 294, 200, ryabImage01, "ENGLISH", "RUSSIAN", "HEBREW", 0);
+    s1.display();
   }
-
+}
+class Slide
+{
+  constructor(x, y, width, height, img, English, Russian, Hebrew, index)
+  {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.img = img;
+    this.English = English;
+    this.Russian = Russian;
+    this.Hebrew = Hebrew;
+  }
+  display()
+  {
+    image(this.img, this.x, this.y, this.width, this.height);
+  }
 }
