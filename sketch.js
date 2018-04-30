ef3e718bf5a188bd187f6c5461f6277eb0815084
Carlos Michael Rodriguez <cmr624@nyu.edu>
2018-04-30 13:27:14 -0400

    fixed, pretty final

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ let plane;
 let index;
 let slideArr = [];
 let currentSlide;
-let s1English, s2English, s2Russian, s2Hebrew, s3English, s3Russian, s3Hebrew, s4English, s4Russian, s4Hebrew;
+let s1English, s2English, s3English, s4English;
 
 function preload()
 {
@@ -52,34 +52,33 @@ function setup() {
   //s1Hebrew = "\t\tיבגניה סמוילובנה ריאבאיה נולדה ב - 6 במרס 1919 בריבניטה, מולדובה. לאחר שסיימה שנתיים בבית הספר לרפואה בדנייפרופטרובסק, פרצה המלחמה והיא פונתה לסוורדלובסק [יקטרינבורג] אשר המשיכה את לימודיה והחלה לעבוד כמנתחת.";
 
   s2English = "\t\tRyabaya was drafted in 1942 and served in the medical-sanitary battalion in the 183rd Rifle Division of the 38th Army (Pictured here). She performed amputations and other surgeries in dugouts and in tents at the Battle of Kursk, near Kharkiv, in western Ukraine, and in Poland."
-  s2Russian = "\t\tВ 1942 году призвана в армию и направлена в медсанбат стрелковой дивизии, где работала хирургом.  Оперировала раненых в землянках и палатках на Курской дуге, под Харьковом, в Западной Украине, Польше. Вот Рябая на этой групповой фотографии с фронта. ";
-  s2Hebrew = "\t\tהיא גויסה ב - 1942 ושירתה בגדוד הרפואי - סניטרי בחטיבת הרובים ה - 183 של הצבא ה - 38. ריאבאיה ביצעה קטיעות וניתוחים אחרים בתעלות ובאוהלים בקרב על קורסק, ליד חרקוב, במערב אוקראינה, ובפולין. הנה היא מופיעה עם חובשים אחרים בבית החולים שדה של חטיבת הרובים 183. על פי כתובת על הגב, התצלום צולם בחזית האוקראינית הרביעית.";
+  //s2Russian = "\t\tВ 1942 году призвана в армию и направлена в медсанбат стрелковой дивизии, где работала хирургом.  Оперировала раненых в землянках и палатках на Курской дуге, под Харьковом, в Западной Украине, Польше. Вот Рябая на этой групповой фотографии с фронта. ";
+  //s2Hebrew = "\t\tהיא גויסה ב - 1942 ושירתה בגדוד הרפואי - סניטרי בחטיבת הרובים ה - 183 של הצבא ה - 38. ריאבאיה ביצעה קטיעות וניתוחים אחרים בתעלות ובאוהלים בקרב על קורסק, ליד חרקוב, במערב אוקראינה, ובפולין. הנה היא מופיעה עם חובשים אחרים בבית החולים שדה של חטיבת הרובים 183. על פי כתובת על הגב, התצלום צולם בחזית האוקראינית הרביעית.";
 
   s3English = "\t\tRyabaya participated in many battles throughout the duration of World War II. Here is a hand-drawn combat map of the battle dates and locations of Ryabaya's unit, the 183rd Rifle Division. Notice how the map traces a path from Riga to Kiev, and ends at Prague in June of 1945.";
-  s3Russian = "\t\tРябая участвовала во многих битвах во время Второй мировой войны. В этой рукописной карте вы можете проследовать по пути 183 стрелковой дивизии и читать информацию о датах и местах боев.";
-  s3Hebrew = "\t\t";
+  //s3Russian = "\t\tРябая участвовала во многих битвах во время Второй мировой войны. В этой рукописной карте вы можете проследовать по пути 183 стрелковой дивизии и читать информацию о датах и местах боев.";
+  //s3Hebrew = "\t\tריאבאיה השתתפה בקרבות רבים לאורך כל מלחמת העולם השנייה. הנה מפה מצוירת ביד של תאריכי הקרב ואת מיקומו של יחידת של ריאבאיה, חטיבת הרובים 183. שים לב איך המפה רשמים נתיב מריגה לקייב, ומ";
 
   s4English = '\t\tUpon enduring a concussion at the close of the war, Ryabaya returned to Ukraine, and later emigrated to the United States. Pictured here is a newspaper article entitled \"Women and War\" published in the Russian-language American newspaper \"Forum.\"';
-  s4Russian = "\t\tריאבאיה השתתפה בקרבות רבים לאורך כל מלחמת העולם השנייה. הנה מפה מצוירת ביד של תאריכי הקרב ואת מיקומו של יחידת של ריאבאיה, חטיבת הרובים 183. שים לב איך המפה רשמים נתיב מריגה לקייב, ומסתיים בפראג ביוני 1945.";
-  s4Hebrew = "\t\t";
+  //s4Hebrew = '\t\tבתמונה מופיעה מאמר בשם "\נשים ומלחמה" שפורסמה בעיתון אמריקאי בשם "פורום" בשפה הרוסית. כאן, ריאבאיה מדברת באופן אישי על חוויותיה כחיילת במלחמת העולם השנייה.';
 
-  s1 = new Slide(3.04, 4.84, "ryab01", s1English, s1Russian, s1Hebrew, 0);
+  s1 = new Slide(3.04, 4.84, "ryab01", s1English, s1Russian, s1Hebrew, "https://bafdigital.org/item/13654");
   slideArr.push(s1);
 
-  s2 = new Slide(4.63, 3.01, "ryab02", s2English, s2Russian, s2Hebrew, 1);
+  s2 = new Slide(4.63, 3.01, "ryab02", s2English, s2Russian, s2Hebrew, "https://bafdigital.org/item/13656");
   slideArr.push(s2);
 
-  s3 = new Slide(4.95, 3.37, "ryab03", s3English, s3Russian, s3Hebrew, 2);
+  s3 = new Slide(4.95, 3.37, "ryab03", s3English, s3Russian, s3Hebrew, "https://bafdigital.org/item/13659");
   slideArr.push(s3);
 
-  s4 = new Slide(3.37, 5.17, "ryab04", s4English, s4Russian, s4Hebrew, 3);
+  s4 = new Slide(3.37, 5.17, "ryab04", s4English, s4Russian, s4Hebrew, "https://bafdigital.org/item/13658");
   slideArr.push(s4);
 
   index = 0;
   currentSlide = slideArr[index];
   //DATA
   plane = new Plane({
-    x:3, y:0, z:-4, rotationX:-90, width: currentSlide.width, height: currentSlide.height, asset:currentSlide.asset
+    x:3, y:0, z:-4, rotationX:-90, width: currentSlide.width, height: currentSlide.height, asset:currentSlide.asset, clickFunction: function() { open(currentSlide.url);}
   });
   markerHiro.addChild(plane);
 }
@@ -96,12 +95,12 @@ function draw() {
     rectMode(CENTER);
     textSize(15);
 
-    rect(posTextBoxX, posTextBoxY, .55 * width, .4 * height, 15);
+    rect(posTextBoxX, posTextBoxY, .55 * width, .5 * height, 15);
 
     fill(0);
     textAlign(LEFT);
     //textFont(fontRussian);
-    text(currentSlide.currentLang, width/2, .888 * height, 400, 200);
+    text(currentSlide.currentLang, width/2, .85 * height, 400, 200);
     imageMode(CENTER);
     //fuck this
 
@@ -136,7 +135,7 @@ function change()
   markerHiro.removeChild(plane);
   currentSlide = slideArr[index];
   plane = new Plane({
-    x:3, y:0, z:-4, rotationX:-90, width: currentSlide.width, height: currentSlide.height, asset:currentSlide.asset
+    x:3, y:0, z:-4, rotationX:-90, width: currentSlide.width, height: currentSlide.height, asset:currentSlide.asset, clickFunction: function() { open(currentSlide.url)}
   });
   markerHiro.addChild(plane);
 
@@ -184,7 +183,7 @@ function mousePressed() {
 }
 class Slide
 {
-  constructor(width, height, asset, English, Russian, Hebrew, index)
+  constructor(width, height, asset, English, Russian, Hebrew, url)
   {
     this.width = width;
     this.height = height;
@@ -193,6 +192,7 @@ class Slide
     this.Russian = Russian;
     this.Hebrew = Hebrew;
     this.currentLang = English;
+    this.url = url;
   }
 
   determineLanguage(){
